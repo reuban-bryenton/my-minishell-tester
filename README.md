ae260454686c256527238b22dc702aa18654f106
Reuban Bryenton <r.bryenton2003@gmail.com>
2023-02-20 16:19:50 +0100

    Init v1.1

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # my-minishell-tester
  I made this tool to test the minishell1, 2 and 42sh projects in my first year at epitech. Feel free to share and use it yourself.
+
+## Current tests
+ 🗒️ Please only use this as a guide and note that this is not an official testing tool.
+ Currently I only have the tests for minishell1 and it is currently incomplete.
+ In version 1.2 I will update the readme to contain info on how to add your own tests.
+
+## installation
+
+ To use this script copy your binary from your minishell project (in this case the example will be mysh) and run the following code:
+
+ ``python3 minishell1_tester.py ./mysh``
+
+ and to get help use:
+
+ ``python3 minishell1_tester.py -h``
+
+<br>
+
+ The output should look simalar to this:
+
+ ``Testing [...] tests...``
+
+ ``✅ Test [...]: Test passed.``
+
+ ``...``
+
+ ``✅ Passed: ...``
+
+ ``❌ Failed: ...``
---
+++ minishell1_tester.py
@@ -0,0 +1,111 @@
+import json
+import subprocess
+import sys
+import colorama
+from colorama import Fore
+
+def reset_color():
+    print(Fore.RESET)
+
+def print_diff(mot, tmot):
+    print(Fore.YELLOW, "---MYSH---")
+    print(mot)
+    print(Fore.YELLOW, "---TCSH---")
+    print(tmot)
+    print(Fore.YELLOW, "---END---")
+    reset_color()
+
+def exec_process(test, md):
+    try:
+        cmds = test['commands']
+        cmds.insert(0, sys.argv[1])
+        bf = ""
+        for cmd in cmds:
+            bf += "echo -e '{0}'\n".format(cmd)
+            if test['sleeptime'] > 0:
+                bf += "sleep {0}\n".format(test['sleeptime'])
+        tst_file = open("tmp", "w")
+        tst_file.write(bf)
+        tst_file.close()
+        eproc = subprocess.Popen(("/bin/bash", "-C", "tmp"), stdout=subprocess.PIPE)
+        exc = sys.argv[1] if not md else "tcsh"
+        proc = subprocess.Popen((exc), stdout=subprocess.PIPE, stderr=subprocess.STDOUT, stdin=eproc.stdout)
+        proc.wait(10)
+        ot = proc.stdout.read()
+        return proc.returncode, ot
+    except subprocess.TimeoutExpired:
+        return 139, None
+    except Exception as e:
+        return -1, None
+
+def start_test(test):
+    mrcode, mot = exec_process(test, False)
+    trcode, tmot = exec_process(test, True)
+    expcode = test['override_excode']
+    if expcode >= 0 and mrcode != expcode and expcode != 139:
+        print(Fore.RED, "❌ Test [{0}]: Test failed.".format(test['name']))
+        print(Fore.BLUE, "ℹ️  expexted: {0} but got: {1}".format(expcode, mrcode))
+        reset_color()
+        return False
+    elif expcode < 0 and (mrcode == 139 or mrcode == 11):
+        print(Fore.MAGENTA, "❌ Test [{0}]: Test crashed.".format(test['name']))
+        reset_color()
+        return False
+    if mrcode == -1:
+        print(Fore.RED, "❌ Test [{0}]: Test failed. Unable to start.".format(test['name']))
+        reset_color()
+        return False
+    if mot != tmot:
+        print(Fore.RED, "❌ Test [{0}]: Test failed. Difference.".format(test['name']))
+        print_diff(mot, tmot)
+        return False
+    print(Fore.GREEN, "✅ Test [{0}]: Test passed.".format(test['name']))
+    reset_color()
+    return True
+
+def main():
+    tests = None
+    npassed = 0
+    nfailed = 0
+    ntests = 0
+
+    if len(sys.argv) != 2:
+        print(Fore.RED + "❌ Wrong args (use -h for help)")
+        return 1
+
+    if (sys.argv[1] == "-h"):
+        print(Fore.YELLOW + "\n👋 Welcome to minishell1 tester by @reuban_bryenton\n")
+        print(Fore.YELLOW + "To use this tool type in the following command:")
+        print(Fore.YELLOW + "\t'python3 minishell1_tester.py ./{binary name}'\n")
+        return 0
+
+    try:
+        fl = open("tests.json")
+        cnt = fl.read()
+        tests = json.loads(cnt)
+    except IOError as e:
+        print(Fore.RED + "❌ Unable to open tests.json: no such file or permission denied.")
+        return 1
+
+    if tests is None:
+        print(Fore.RED + "❌ Unable to load tests from tests.json.")
+        return 1
+
+    
+    for q in tests:
+        ntests += 1
+    print(Fore.CYAN + "\nTesting [{0}] tests...\n".format(ntests))
+
+    for q in tests:
+        if start_test(q):
+            npassed += 1
+        else:
+            nfailed += 1
+
+    print(Fore.GREEN, "✅ Passed: {0}".format(npassed))
+    print(Fore.RED, "❌ Failed: {0}\n".format(nfailed))
+    reset_color()
+    return 0
+
+if __name__ == "__main__":
+    exit(main())
